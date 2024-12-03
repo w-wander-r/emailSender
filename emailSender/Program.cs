9e4fee74e8dd98aea0a0e734ac08c135407d0ec5
w-wander-r <wanderexe68@gmail.com>
2024-12-03 14:01:17 +0200

    Add files via upload

---
+++ emailSender/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace SendEmailWithGoogleSMTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fromMail = "FromEmail@xxxx.com";
+            string fromPassword = "yourAppPassword";
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.Subject = "Test Subject";
+            message.To.Add(new MailAddress("ToEmail@xxxx.com"));
+            message.Body = "<html><body> TEXT HERE </body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587, 
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+
+            smtpClient.Send(message);
+        }
+    }
+}
